600eaa976be1c3e2eaa3068c4a2fb684513a32fc
ahmedDoro <a.yusuf-doro@rgu.ac.uk>
2017-05-05 03:46:54 +0100

    Update dbconfig.php

--- api/src/connection/dbconfig.php
+++ api/src/connection/dbconfig.php
@@ -1,10 +1,10 @@
 <?php
 
 class db{
-    private $dbhost = 'localhost';
-    private $dbuser = 'root';
-    private $dbpass = '';
-    private $dbname = 'themalamsclothing';
+    private $dbhost = 'br-cdbr-azure-south-b.cloudapp.net';
+    private $dbuser = 'b5878316b539fe';
+    private $dbpass = '8e5d969e';
+    private $dbname = 'campusdate';
 
     public function connect(){
         $mysql_connect_str = "mysql:host=$this->dbhost; dbname=$this->dbname";
