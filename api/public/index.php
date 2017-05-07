d2acdf083a436f22d52f28dbde832510d097d9c2
ahmedDoro <a.yusuf-doro@rgu.ac.uk>
2017-05-07 05:38:27 +0100

    Update index.php

--- api/public/index.php
+++ api/public/index.php
@@ -2,7 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-//require '../vendor/autoload.php';
+require '../vendor/autoload.php';
 require '../src/connection/dbconfig.php';
 
 $app = new \Slim\App;
@@ -16,3 +16,4 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
 // Products routes
 require '../src/routes/products.php';
 $app->run();
+?>
