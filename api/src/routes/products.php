a64a06c6031f5a88ac4bc76c625d3cbcd65af8a9
ahmedDoro <a.yusuf-doro@rgu.ac.uk>
2017-05-08 00:31:09 +0100

    Update products.php

--- api/src/routes/products.php
+++ api/src/routes/products.php
@@ -1,12 +1,12 @@
 <?php
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
+use ..\Psr\Http\Message\ServerRequestInterface as Request;
 
-use \Psr\Http\Message\ResponseInterface as Response;
+use ..\Psr\Http\Message\ResponseInterface as Response;
 
 
 
-$app = new \Slim\App;
+$app = new ..\Slim\App;
 
 
 
