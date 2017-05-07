ba41b7039614b618822c32c5cd6ba54fa7f67fc0
ahmedDoro <a.yusuf-doro@rgu.ac.uk>
2017-05-07 06:25:46 +0100

    Update index.php

--- api/public/index.php
+++ api/public/index.php
@@ -1,20 +1,3 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-
-//require '../vendor/autoload.php';
-require '../src/config/db.php';
-
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
-
-    return $response;
-});
-
-// products Routes
-//require '../src/routes/products.php';
-
-$app->run();
+eho "Hello world!";
 ?>
