28ff8e8ade7e1a5a2fb018cc3824686b88dd3f14
ahmedDoro <a.yusuf-doro@rgu.ac.uk>
2017-05-08 01:10:05 +0100

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,18 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+require '../src/connection/dbconfig.php';
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+// Products routes
+require '../src/routes/products.php';
+$app->run();
