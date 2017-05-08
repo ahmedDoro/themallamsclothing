f9db74c083df3a8b55bc2b556b96025fba61eec0
ahmedDoro <a.yusuf-doro@rgu.ac.uk>
2017-05-08 01:11:08 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,8 +2,8 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
-require '../src/connection/dbconfig.php';
+require 'vendor/autoload.php';
+require 'src/connection/dbconfig.php';
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
