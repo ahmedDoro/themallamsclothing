ee436cb0b0b9c730f04cfd2a03621591bf2f6d2b
ahmedDoro <a.yusuf-doro@rgu.ac.uk>
2017-05-08 01:00:02 +0100

    Update index.php

--- api/public/index.php
+++ api/public/index.php
@@ -1,21 +1,27 @@
 <?php
-use ..\vendor\Psr\Http\Message\ServerRequestInterface as Request;
-use ..\vendor\Psr\Http\Message\ResponseInterface as Response;
-
-require '../vendor/autoload.php';
-require '../src/config/db.php';
-
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
-
-    return $response;
-});
-
-// products Routes
-require '../src/routes/products.php';
-
-$app->run();
-
-?>
+	// Include confi.php
+	include_once('../config.php');
+	
+	
+	$action = $_SERVER['REQUEST_METHOD'];
+	
+	if ($action == 'GET'){
+			
+			 $getdata = "SELECT * FROM  products ";
+		               
+						$qur = $conn->query($getdata);
+						$msg="";
+						while($r = mysqli_fetch_assoc($qur)){
+						 
+						$msg[] = array("Image:" => $r['product_image'], "Title:" => $r['product_title'], "Price" => $r['product_price']);
+						}
+						$json = $msg;
+						 
+						header('content-type: application/json');
+						echo json_encode($json);
+						 
+						@mysqli_close($conn);
+			
+		
+	}
+ ?>
