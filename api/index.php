adba172d49b6cbcc151589e24e13a11332701a1e
ahmedDoro <a.yusuf-doro@rgu.ac.uk>
2017-05-08 01:02:32 +0100

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,27 @@
+<?php
+	// Include confi.php
+	include_once('../dbconfig.php');
+	
+	
+	$action = $_SERVER['REQUEST_METHOD'];
+	
+	if ($action == 'GET'){
+			
+			 $getdata = "SELECT * FROM  products ";
+		               
+						$qur = $conn->query($getdata);
+						$msg="";
+						while($r = mysqli_fetch_assoc($qur)){
+						 
+						$msg[] = array("Image:" => $r['product_image'], "Title:" => $r['product_title'], "Price" => $r['product_price']);
+						}
+						$json = $msg;
+						 
+						header('content-type: application/json');
+						echo json_encode($json);
+						 
+						@mysqli_close($conn);
+			
+		
+	}
+ ?>
