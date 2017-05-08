81e72aa96b86176fc4548ab11702d61765614859
ahmedDoro <a.yusuf-doro@rgu.ac.uk>
2017-05-08 01:12:46 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use \vendor\Psr\Http\Message\ServerRequestInterface as Request;
+use \vendor\Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 require 'src/connection/dbconfig.php';
